4334d1f5434a0a162fae14a1b26ef18278478ebb
dzhit86 <dzhit86@gmail.com>
2021-03-08 19:16:37 +0600

    Сделано адаптивное меню + правки адаптива по первым двум экранам

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,4 +18,57 @@ $(document).ready(function () {
 
 	});
 
+	// Главное меню
+	$(function () {
+		let menu = $('.mob-menu'),
+			menuBtn = $('#btnBurger');
+		menuBtn.on('click', function (e) {
+			e.preventDefault();
+			if (menuBtn.hasClass('btn-burger--cross')) {
+				displayMenu('hide');
+			} else {
+				displayMenu('show');
+			}
+		});
+	});
+
+
+});
+
+// Отображение меню
+function displayMenu(status = false) {
+	let menu = $('.mob-menu'),
+		menuBtn = $('#btnBurger');
+	if (status === 'show') {
+		animationMenu(menu, 'left');
+		menuBtn.addClass('btn-burger--cross');
+		$('body').addClass('fixed');
+		return true;
+	}
+	if (status === 'hide') {
+		animationMenu(menu, 'right');
+		menuBtn.removeClass('btn-burger--cross');
+		$('body').removeClass('fixed');
+		return true;
+	}
+	if (status === false && menu.is(':visible')) {
+		return true;
+	}
+	return false;
+}
+
+// Анимация меню
+function animationMenu(id, direction) {
+	if (direction == "left") {
+		id.css('transform', 'translateX(-100%)');
+	}
+	if (direction == "right") {
+		id.css('transform', 'translateX(0)');
+	}
+}
+
+$(window).resize(function (params) {
+	if ($(window).outerWidth() > 700 && $('.mob-menu').css('transform') == 'translateX(0)') {
+		$('.mob-menu').css('transform', 'translateX(100%)');
+	}
 });
